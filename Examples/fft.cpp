12d0ddab801747a9434aa75d97facd7b19f446c7
Claudio Martino <claudiomartino.it@gmail.com>
2024-08-23 19:07:35 +0200

    Added radix 8 feature to FFT example

--- Examples/fft.cpp
+++ Examples/fft.cpp
@@ -35,24 +35,6 @@ void ref_dft(const std::vector<Cpx<T>> x, std::vector<Cpx<T>>& y, const size_t N
 }
 
 template <typename T>
-void reverse_reorder(std::vector<Cpx<T>>& x, size_t N, size_t r) {
-  Cpx<T> tmp;
-  for(size_t idx_src=1; idx_src<N-1; idx_src++) { // 1st and last never change
-    size_t i=1; size_t j=N/r;
-    size_t idx_dst=0;
-    for (; i<N; i = i * r, j = j / r) {
-      idx_dst += j * ( int(idx_src / i) % r );
-    }
-    // Swap
-    if(idx_dst > idx_src) { 
-      tmp = x[idx_src];
-      x[idx_src] = x[idx_dst];
-      x[idx_dst] = tmp;
-    }
-  }
-}
-
-template <typename T>
 struct Bfly {
   virtual ~Bfly() = default;
   virtual void run(Cpx<T>* data, size_t idx0, size_t step) = 0;
@@ -102,9 +84,104 @@ struct Bfly4 : Bfly<T> {
 };
 
 template <typename T>
+struct Bfly8 : Bfly<T> {
+  void run(Cpx<T>* data, size_t idx0, size_t step) override {
+    size_t idx1 = idx0 + step;
+    size_t idx2 = idx0 + 2*step;
+    size_t idx3 = idx0 + 3*step;
+    size_t idx4 = idx0 + 4*step;
+    size_t idx5 = idx0 + 5*step;
+    size_t idx6 = idx0 + 6*step;
+    size_t idx7 = idx0 + 7*step;
+
+    Cpx<T> in0 = data[idx0]; 
+    Cpx<T> in1 = data[idx1]; 
+    Cpx<T> in2 = data[idx2]; 
+    Cpx<T> in3 = data[idx3]; 
+    Cpx<T> in4 = data[idx4]; 
+    Cpx<T> in5 = data[idx5]; 
+    Cpx<T> in6 = data[idx6]; 
+    Cpx<T> in7 = data[idx7]; 
+
+    Cpx<T> rot45 = {sqrt(2)/2, sqrt(2)/2}; 
+
+    Cpx<T> out0 = in0 + in1 + in2 + in3
+                + in4 + in5 + in6 + in7; 
+    Cpx<T> out1  = in0;
+           out1 += in1.rot270() * rot45;
+           out1 += in2.rot270();
+           out1 -= in3 * rot45; // += in3.rot180() * rot45;
+           out1 -= in4;
+           out1 += in5.rot90() * rot45;
+           out1 += in6.rot90();
+           out1 += in7 * rot45;
+    Cpx<T> out2  = in0;
+           out2 += in1.rot270();
+           out2 -= in2;
+           out2 += in3.rot90();
+           out2 += in4;
+           out2 += in5.rot270();
+           out2 -= in6;
+           out2 += in7.rot90();
+    Cpx<T> out3  = in0;
+           out3 -= in1 * rot45; // += in1.rot180() * rot45;
+           out3 += in2.rot90();
+           out3 += in3.rot270() * rot45;
+           out3 -= in4;
+           out3 += in5 * rot45;
+           out3 += in6.rot270();
+           out3 += in7.rot90() * rot45;
+    Cpx<T> out4  = in0;
+           out4 -= in1;
+           out4 += in2;
+           out4 -= in3;
+           out4 += in4;
+           out4 -= in5;
+           out4 += in6;
+           out4 -= in7;
+    Cpx<T> out5  = in0;
+           out5 += in1.rot90() * rot45;
+           out5 += in2.rot270();
+           out5 += in3 * rot45;
+           out5 -= in4;
+           out5 += in5.rot270() * rot45;
+           out5 += in6.rot90();
+           out5 -= in7 * rot45; // += in7.rot180() * rot45;
+    Cpx<T> out6  = in0;
+           out6 += in1.rot90();
+           out6 -= in2;
+           out6 += in3.rot270();
+           out6 += in4;
+           out6 += in5.rot90();
+           out6 -= in6;
+           out6 += in7.rot270();
+    Cpx<T> out7  = in0;
+           out7 += in1 * rot45;
+           out7 += in2.rot90();
+           out7 += in3.rot90() * rot45;
+           out7 -= in4;
+           out7 -= in5 * rot45; // += in5.rot180() * rot45;
+           out7 += in6.rot270();
+           out7 += in7.rot270() * rot45;
+
+    data[idx0] = out0;
+    data[idx1] = out1;
+    data[idx2] = out2;
+    data[idx3] = out3;
+    data[idx4] = out4;
+    data[idx5] = out5;
+    data[idx6] = out6;
+    data[idx7] = out7;
+  };
+};
+
+template <typename T>
 std::unique_ptr<Bfly<T>> get_butterfly(size_t size) {
   if(size == 4) {
     return std::unique_ptr<Bfly4<T>>(new Bfly4<T>);
+  }
+  else if(size == 8) {
+    return std::unique_ptr<Bfly8<T>>(new Bfly8<T>);
   }
   else { // default: radix-2
     return std::unique_ptr<Bfly2<T>>(new Bfly2<T>);
@@ -142,6 +219,24 @@ void fft(Cpx<T>* data, size_t N, size_t r) {
           }
         }
       }
+    }
+  }
+}
+
+template <typename T>
+void reverse_reorder(std::vector<Cpx<T>>& x, size_t N, size_t r) {
+  Cpx<T> tmp;
+  for(size_t idx_src=1; idx_src<N-1; idx_src++) { // 1st and last never change
+    size_t i=1; size_t j=N/r;
+    size_t idx_dst=0;
+    for (; i<N; i = i * r, j = j / r) {
+      idx_dst += j * ( int(idx_src / i) % r );
+    }
+    // Swap
+    if(idx_dst > idx_src) { 
+      tmp = x[idx_src];
+      x[idx_src] = x[idx_dst];
+      x[idx_dst] = tmp;
     }
   }
 }
--- README.md
+++ README.md
@@ -5,20 +5,6 @@ A custom complex arithmetic library implemented in c++, for those who cannot rel
 ## Getting Started
 Add the `complex.hpp` file to your project and start to code using the `Cpx<T>` class.
 
-## Examples
-### FFT
-The FFT has been implemented. To build and run it:
-```
-make fft
-./fft N R
-```
-where `N` is the FFT size (default is 1024) and R is the radix size (2 or 4, default is 2). N must be a power of R.
-
-* Add the `-DBENCH` flag to measure the execution time, too.
-* Add the `-DSAVE` flag to save the input and the output signal in `.txt` files. You can plot them using `plot.py`, you will need [matplotlib](https://matplotlib.org/).
-
-![FFT plots](Examples/figure_1.png)
-
 ## Test
 To run the test routines:
 ```
@@ -26,7 +12,21 @@ make test
 ./test
 ```
 
+## Examples
+### FFT
+The FFT has been implemented. To build and run it:
+```
+make fft
+./fft N R
+```
+where `N` is the FFT size (default is 1024) and R is the radix size (2, 4, or 8, default is 2). N must be a power of R.
+
+* Add the `-DBENCH` flag to measure the execution time, too.
+* Add the `-DSAVE` flag to save the input and the output signal in `.txt` files. You can plot them using `plot.py`, you will need [matplotlib](https://matplotlib.org/).
+
+![FFT plots](Examples/figure_1.png)
+
 ## Version History
 * 0.1
-    * Initial Release
+    * Initial release
 
