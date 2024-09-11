158a336ff50fe2c8233160344e184654b6e347db
Claudio Martino <claudiomartino.it@gmail.com>
2024-09-11 20:54:56 +0200

    Added cyclic shift and sampling freq in FFT plot script and reduced number of plots

--- Examples/plot.py
+++ Examples/plot.py
@@ -1,5 +1,14 @@
+#usage python3 plot.py [sample_freq]
+
 import matplotlib.pyplot as plt
 import math 
+import sys
+
+def cyclic_shift(y):
+  y_1 = y[:len(y)//2]
+  y_2 = y[len(y)//2:]
+
+  return y_2 + y_1
 
 def conv(x):
   x_r   = [0] * len(x)
@@ -17,30 +26,42 @@ def conv(x):
 
 # MAIN
 
-# Read signals from files
+sample_freq = 1
+
+if len(sys.argv) >= 2:
+    sample_freq = int(sys.argv[1])
+print(sample_freq)
+
+# Read signals from files (assumption: one sample per second)
 x_file = open("input.txt", "r")
 x = x_file.readlines()
 x_r, x_i, x_abs = conv(x)
 
 y_file = open("output.txt", "r")
 y = y_file.readlines()
+y = cyclic_shift(y)
 y_r, y_i, y_abs = conv(y)
 
+# Define x axis
+samples = range(len(x))
+
+time = [x / sample_freq for x in samples]
+
+freq = [0] * len(time) 
+for t in range(len(time) // 2):
+  f = (samples[t] * sample_freq / float(len(time)))
+  freq[    t + len(time) // 2] = f
+  freq[-t -1 + len(time) // 2] = -f
+
 # Plot
-fig, axs = plt.subplots(6, sharex=True)
+fig, axs = plt.subplots(1,2)
 
-axs[0].plot(x_r)
-axs[1].plot(x_i)
-axs[2].plot(x_abs)
-axs[0].set_title('re(x[n])')
-axs[1].set_title('im(x[n])')
-axs[2].set_title('abs(x[n])')
+axs[0].plot(time, x_abs)
+axs[0].set_title('abs(x[n])')
+axs[0].set(xlabel='s')
 
-axs[3].plot(y_r)
-axs[4].plot(y_i)
-axs[5].plot(y_abs)
-axs[3].set_title('re(y[n])')
-axs[4].set_title('im(y[n])')
-axs[5].set_title('abs(y[n])')
+axs[1].plot(freq, y_abs)
+axs[1].set_title('abs(y[n])')
+axs[1].set(xlabel='Hz')
 
 plt.show()
