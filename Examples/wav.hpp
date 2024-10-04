0a57f13ea45903ee55408a48fd6c9b1ba4a32be0
Claudio Martino <claudiomartino.it@gmail.com>
2024-10-04 15:48:46 +0200

    Added some control in wav parser

--- Examples/wav.hpp
+++ Examples/wav.hpp
@@ -9,21 +9,21 @@
 // see: http://soundfile.sapp.org/doc/WaveFormat/
 struct WavHeader {
   // RIFF chunk descriptor
-  char riff[4];                  // ChunkID (RIFF string)
-  unsigned int chunk_size;       // ChunkSize (overall size of file in bytes)
-  char wave[4];                  // Format (WAVE string)
+  char riff[4];                  // 'RIFF' string
+  unsigned int chunk_size;       // size of the rest of the file (i.e. file size - 8 bytes)
+  char wave[4];                  // 'WAVE' string
   // fmt sub-chunk
-  char subchunk1_id[4];          // fmt string with trailing null char
-  unsigned int subchunk1_size;   // length of the format data
-  unsigned int audio_format;     // format type: 1- PCM, 3- IEEE float, 6- 8bit A law, 7- 8bit mu law
-  unsigned int num_channels;     // number of channels
+  char subchunk1_id[4];          // 'fmt ' string (with trailing null char)
+  unsigned int subchunk1_size;   // size of the rest of the sub-chunk (i.e. sub-chunk size - 8 bytes)
+  unsigned int audio_format;     // format type: 1=PCM, 3=IEEE float, 6=8bit A law, 7=8bit mu law
+  unsigned int num_channels;     // number of channels: 1=mono, 2=stereo
   unsigned int sample_rate;      // sampling rate (blocks per second)
   unsigned int byte_rate;        // sample_rate * num_channels * bits_per_sample / 8
   unsigned int block_align;      // num_channels * bits_per_sample / 8
   unsigned int bits_per_sample;  // bits per sample (8, 16...)
   // data sub-chunk
-  char subchunk2_id[4];          // DATA string or FLLR string
-  unsigned int subchunk2_size;   // num_samples * num_channels * bits_per_sample / 8
+  char subchunk2_id[4];          // 'DATA' string or 'FLLR' string
+  unsigned int subchunk2_size;   // size of data in bytes: num_samples * num_channels * bits_per_sample / 8
 
   // Little endian values
   char little_chunk_size[4];
@@ -54,7 +54,7 @@ void read_wav_header(std::ifstream& fs, WavHeader& header, bool verbose) {
   fs.read(header.little_chunk_size, 4);
   header.chunk_size = little_to_big_endian_4((unsigned char *)header.little_chunk_size);
   if(verbose)
-    std::cout << "[ 4- 7] Chunk Size: " << header.chunk_size << " bytes (" << header.chunk_size/1024 << " KB)" << std::endl;
+    std::cout << "[ 4- 7] Chunk Size: " << header.chunk_size << " bytes (" << header.chunk_size / 1024 << " KB)" << std::endl;
 
   fs.read(header.wave, sizeof(header.wave));
   if(verbose)
@@ -95,13 +95,13 @@ void read_wav_header(std::ifstream& fs, WavHeader& header, bool verbose) {
 
   fs.read(header.little_sample_rate, 4);
   header.sample_rate = little_to_big_endian_4((unsigned char *)header.little_sample_rate);
-  if(verbose)
+  //if(verbose)
     std::cout << "[24-27] Sample Rate: " << header.sample_rate << std::endl;
 
   fs.read(header.little_byte_rate, 4);
   header.byte_rate = little_to_big_endian_4((unsigned char *)header.little_byte_rate);
   if(verbose)
-    std::cout << "[28-31] Byte Rate: " << header.byte_rate << " Bit Rate: " << header.byte_rate * 8 << std::endl;
+    std::cout << "[28-31] Byte Rate: " << header.byte_rate << " (Bit Rate: " << header.byte_rate * 8 << ")" << std::endl;
 
   fs.read(header.little_block_align, 2);
   header.block_align = little_to_big_endian_2((unsigned char *)header.little_block_align);
@@ -121,17 +121,43 @@ void read_wav_header(std::ifstream& fs, WavHeader& header, bool verbose) {
   fs.read(header.little_subchunk2_size, 4);
   header.subchunk2_size = little_to_big_endian_4((unsigned char *)header.little_subchunk2_size);
   if(verbose)
-    std::cout << "[40-43] Subchunk2 Size: " << header.subchunk2_size << std::endl;
+    std::cout << "[40-43] Subchunk2 Size: " << header.subchunk2_size << " bytes (" << header.subchunk2_size / 1024 << " KB)" << std::endl;
+
+  // Check some values
+  if(header.audio_format == 1) {
+    if(header.subchunk1_size != 16) {
+      std::cout << "Error in the header sub-chunk 1 size for PCM." << std::endl;
+      exit(1);
+    }
+  }
+
+  if(header.byte_rate != header.sample_rate * header.num_channels * header.bits_per_sample / 8) {
+    std::cout << "Error in the header byte rate." << std::endl;
+    exit(1);
+  }
+  if(header.block_align != header.num_channels * header.bits_per_sample / 8) {
+    std::cout << "Error in the header block align." << std::endl;
+    exit(1);
+  }
+
+  if(header.chunk_size != 4 + (8 + header.subchunk1_size) + (8 + header.subchunk2_size)) {
+    std::cout << "Error in the header sub-chunk 2 size." << std::endl;
+    exit(1);
+  }
 }
 
 void compute_wave_sample_sizes(WavHeader& header, long& num_samples, long& size_of_each_sample, bool verbose) {
-  num_samples = (8 * header.subchunk2_size) / (header.num_channels * header.bits_per_sample);
+  long bits_per_channel = 8 * header.subchunk2_size;
+  long num_samples_per_channel = bits_per_channel / header.bits_per_sample;
+  num_samples = num_samples_per_channel / header.num_channels;
   if(verbose)
     std::cout << "Number of samples: " << num_samples << std::endl;
-  size_of_each_sample = (header.num_channels * header.bits_per_sample) / 8;
+
+  size_of_each_sample = header.num_channels * header.bits_per_sample / 8;
   if(verbose)
     std::cout << "Size of each sample: " << size_of_each_sample << " bytes" << std::endl;
-  float duration_in_seconds = (float)header.chunk_size / header.byte_rate;
+
+  float duration_in_seconds = (float)header.subchunk2_size / header.byte_rate;
   if(verbose)
     std::cout << "Duration: " << duration_in_seconds << " s" << std::endl;
 } 
@@ -139,18 +165,12 @@ void compute_wave_sample_sizes(WavHeader& header, long& num_samples, long& size_
 void read_pcm_wav_data(std::ifstream& fs, WavHeader& header, long num_samples, long size_of_each_sample, std::vector<Cpx<double>>& out) {
 
   if(header.num_channels > 1) {
-    printf("More than one channel, not supported.\n");
+    std::cout << "More than one channel (" << header.num_channels << "), read not supported for now." << std::endl;
     exit(1);
   }
 
   char data_buffer[size_of_each_sample];
   long bytes_in_each_channel = size_of_each_sample / header.num_channels;
-
-  // Make sure that the bytes-per-sample is completely divisible by num. of channels
-  //if((bytes_in_each_channel * header.num_channels) != size_of_each_sample) {
-  //  printf("Error: %ld x %ud <> %ld\n", bytes_in_each_channel, header.num_channels, size_of_each_sample);
-  //  exit(1);
-  //}
 
   // The valid amplitude range for values based on the bits per sample
   //long low_limit = 0l;
@@ -252,7 +272,7 @@ void write_pcm_wav_data(std::ofstream& fs, WavHeader& header, long num_samples, 
 
 void signal_from_wav_file(std::ifstream& fs, WavHeader& header, std::vector<Cpx<double>>& x) {
   // Read header
-  read_wav_header(fs, header, 1);
+  read_wav_header(fs, header, false);
 
   if (header.audio_format != 1) { // PCM only
     std::cout << "Only PCM is supported." << std::endl;
@@ -261,7 +281,7 @@ void signal_from_wav_file(std::ifstream& fs, WavHeader& header, std::vector<Cpx<
 
   // Compute signal size
   long num_samples, size_of_each_sample;
-  compute_wave_sample_sizes(header, num_samples, size_of_each_sample, 1);
+  compute_wave_sample_sizes(header, num_samples, size_of_each_sample, true);
 
   size_t N = x.size();
   if(N > num_samples) {
